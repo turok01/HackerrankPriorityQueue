6d53a484b74c6a62fe6c826c85b92775e64ca8f7
turok01 <44950560+turok01@users.noreply.github.com>
2020-02-07 11:46:27 +0300

    PriorityQueue Solved

--- src/com/Igor/Main.java
+++ src/com/Igor/Main.java
@@ -20,7 +20,7 @@ class Student{
     public double getCgpa() {
         return cgpa;
     }
-    public void Student(int id, String name, double cgpa){
+    public Student(int id, String name, double cgpa){
         this.id = id;
         this.name = name;
         this.cgpa = cgpa;
@@ -37,9 +37,25 @@ class StudentCgpaComparator implements Comparator<Student>{
                 return 0;
     }
 }
+class StudentCgpaDescendingComparator implements Comparator<Student>{
+    public int compare(Student s1, Student s2){
+        if(s1.cgpa<s2.cgpa)
+            return 1;
+        else
+        if(s1.cgpa>s2.cgpa)
+            return -1;
+        else
+            return 0;
+    }
+}
 class StudentNameComparator implements Comparator<Student>{
     public int compare(Student s1, Student s2){
         return s1.name.compareTo(s2.name);
+    }
+}
+class StudentNameDescendingComparator implements Comparator<Student>{
+    public int compare(Student s1, Student s2){
+        return s2.name.compareTo(s1.name);
     }
 }
 class StudentIdComparator implements Comparator<Student>{
@@ -47,20 +63,42 @@ class StudentIdComparator implements Comparator<Student>{
         return s1.id - s2.id;
     }
 }
+class StudentIdDescendingComparator implements Comparator<Student>{
+    public int compare(Student s1, Student s2){
+        return s2.id - s1.id;
+    }
+}
 class Priorities{
     public List<Student> getStudents(List<String> events){
         int served = 0;
-        Comparator<Student> studentComparator = new StudentCgpaComparator().
+        Comparator<Student> studentComparator = new StudentCgpaComparator();
+        Comparator<Student> studentDescendingComparator = new StudentCgpaDescendingComparator().
                 thenComparing(new StudentNameComparator().
                         thenComparing(new StudentIdComparator()));
         PriorityQueue<Student> studentPriorityQueue = new PriorityQueue<Student>(events.size(),
-                studentComparator);
+                studentDescendingComparator);
         for(String event : events){
-            if(event.equals("SERVED")) served++;
-            //else
-                //event.
+            Scanner sc = new Scanner(event);
+            StringTokenizer st = new StringTokenizer( event," ");
+            String servedToken = st.nextToken();
+            String name;
+            int id;
+            double cgpa;
+            if(servedToken.equals("SERVED")) {
+                if(studentPriorityQueue.size()!=0)
+                    studentPriorityQueue.poll();
+            }
+            else{
+                name = st.nextToken();
+                cgpa = Double.parseDouble(st.nextToken());
+                id = Integer.parseInt(st.nextToken());
+                studentPriorityQueue.add(new Student(id, name, cgpa));
+            }
         }
-        return studentPriorityQueue;
+        List <Student> studentList = new ArrayList<Student>(studentPriorityQueue.size());
+        while(!studentPriorityQueue.isEmpty())
+            studentList.add(studentPriorityQueue.poll());
+        return studentList;
     }
 }
 public class Main {
